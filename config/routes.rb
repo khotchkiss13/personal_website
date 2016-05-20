93aab2ba57f53532f15df698ac40c91c6cbe5675
Kyle Hotchkiss <khotchkiss13@berkeley.edu>
2016-05-19 21:35:14 -0700

    Devise for users, Foundation for grid/css

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  get "/", to: 'user#root', as: 'root'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
